ce27fb4030cae59318a714742e8d3086b4ed1b22
Carlos <carlos.lizgon@gmail.com>
2020-04-07 18:37:21 +0200

    base

--- PokedexC_sharp/VentanaPrincipal.Designer.cs
+++ PokedexC_sharp/VentanaPrincipal.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.nombrePokemon = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(57, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 238);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(57, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "<--";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(178, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "-->";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // nombrePokemon
+            // 
+            this.nombrePokemon.Location = new System.Drawing.Point(269, 33);
+            this.nombrePokemon.Name = "nombrePokemon";
+            this.nombrePokemon.Size = new System.Drawing.Size(167, 47);
+            this.nombrePokemon.TabIndex = 3;
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 424);
+            this.Controls.Add(this.nombrePokemon);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "VentanaPrincipal";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label nombrePokemon;
     }
 }
 
--- PokedexC_sharp/VentanaPrincipal.cs
+++ PokedexC_sharp/VentanaPrincipal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,41 @@ namespace PokedexC_sharp
 {
     public partial class VentanaPrincipal : Form
     {
+
+        Conexion miConexion = new Conexion();
+        DataTable misPokemons = new DataTable();
+        int idActual = 1; //el pokemon que sale
+
         public VentanaPrincipal()
         {
             InitializeComponent();
+        }
+
+        private Image convierteBlobAImagen(byte[] img)
+        {
+            MemoryStream ms = new System.IO.MemoryStream(img);
+            return (Image.FromStream(ms));
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            idActual--;
+            if (idActual <= 0) { idActual = 1; }
+
+            misPokemons = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = misPokemons.Rows[0]["nombre"].ToString();
+            pictureBox1.Image = convierteBlobAImagen((byte[])misPokemons.Rows[0]["imagen"]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            idActual++;
+            if (idActual >= 151) { idActual = 151; }
+
+            misPokemons = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = misPokemons.Rows[0]["nombre"].ToString();
+            pictureBox1.Image = convierteBlobAImagen((byte[])misPokemons.Rows[0]["imagen"]);
         }
     }
 }
